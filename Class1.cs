d61fce3be5a5df63bff74776362251b24a4c479e
ykrop23 <70780982+ykrop23@users.noreply.github.com>
2020-10-09 21:07:46 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+
+    class Adress
+    {
+        public Adress(int index, string country, string city, string street, int house, int apartament)
+        {
+            this.index = index;
+            this.country = country;
+            this.city = city;
+            this.street = street;
+            this.house = house;
+            this.apartament = apartament;
+        }
+
+        public int index { get; set; }
+        public string country { get; set; }
+        public string city { get; set; }
+        public string street { get; set; }
+        public int house { get; set; }
+        public int apartament { get; set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Index: {index}");
+
+            Console.WriteLine($"Country: {country}");
+
+            Console.WriteLine($"City: {city}");
+
+            Console.WriteLine($"Street: {street}");
+
+            Console.WriteLine($"House: {house}");
+
+            Console.WriteLine($"Apartament: {apartament}");
+        }
+    }
+}
