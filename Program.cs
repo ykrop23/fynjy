d289bb4e160820b7666ee30f99bfb5e632407e64
ykrop23 <70780982+ykrop23@users.noreply.github.com>
2020-10-09 21:54:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Address address = new Address();
+
+            address.Country = "Ukraine";
+            address.City = "Kiev";
+            address.Street = "Khreshchatyk Street";
+            address.House = "15-б";
+            address.Apartament = "68";
+            address.Index = "04495";
+
+            Console.WriteLine(address.Country);
+            Console.WriteLine(address.City);
+            Console.WriteLine(address.Street);
+            Console.WriteLine(address.House);
+            Console.WriteLine(address.Apartament);
+            Console.WriteLine(address.Index);
+
+            Console.ReadKey();
+        }
+    }
+}
